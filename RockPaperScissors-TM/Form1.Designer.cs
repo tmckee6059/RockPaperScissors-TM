69314946d884bfd89b151020290721ae10a2e26a
tymck <tymck@MEDIASERVER>
2023-08-26 08:30:57 -0500

    Add project files.

---
+++ RockPaperScissors-TM/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace RockPaperScissors_TM
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.playergroupBox = new System.Windows.Forms.GroupBox();
+            this.aigroupBox = new System.Windows.Forms.GroupBox();
+            this.outcomegroupBox = new System.Windows.Forms.GroupBox();
+            this.versusLabel = new System.Windows.Forms.Label();
+            this.shootButton = new System.Windows.Forms.Button();
+            this.rematchButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.playergroupBox.SuspendLayout();
+            this.aigroupBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // playergroupBox
+            // 
+            this.playergroupBox.Controls.Add(this.pictureBox3);
+            this.playergroupBox.Controls.Add(this.pictureBox2);
+            this.playergroupBox.Controls.Add(this.pictureBox1);
+            this.playergroupBox.Location = new System.Drawing.Point(206, 104);
+            this.playergroupBox.Name = "playergroupBox";
+            this.playergroupBox.Size = new System.Drawing.Size(200, 100);
+            this.playergroupBox.TabIndex = 0;
+            this.playergroupBox.TabStop = false;
+            this.playergroupBox.Text = "You";
+            // 
+            // aigroupBox
+            // 
+            this.aigroupBox.Controls.Add(this.pictureBox4);
+            this.aigroupBox.Controls.Add(this.pictureBox5);
+            this.aigroupBox.Controls.Add(this.pictureBox6);
+            this.aigroupBox.Location = new System.Drawing.Point(611, 104);
+            this.aigroupBox.Name = "aigroupBox";
+            this.aigroupBox.Size = new System.Drawing.Size(200, 100);
+            this.aigroupBox.TabIndex = 1;
+            this.aigroupBox.TabStop = false;
+            this.aigroupBox.Text = "Computer";
+            // 
+            // outcomegroupBox
+            // 
+            this.outcomegroupBox.Location = new System.Drawing.Point(409, 257);
+            this.outcomegroupBox.Name = "outcomegroupBox";
+            this.outcomegroupBox.Size = new System.Drawing.Size(200, 100);
+            this.outcomegroupBox.TabIndex = 2;
+            this.outcomegroupBox.TabStop = false;
+            // 
+            // versusLabel
+            // 
+            this.versusLabel.AutoSize = true;
+            this.versusLabel.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.versusLabel.Location = new System.Drawing.Point(487, 110);
+            this.versusLabel.Name = "versusLabel";
+            this.versusLabel.Size = new System.Drawing.Size(39, 30);
+            this.versusLabel.TabIndex = 3;
+            this.versusLabel.Text = "VS";
+            // 
+            // shootButton
+            // 
+            this.shootButton.Location = new System.Drawing.Point(469, 143);
+            this.shootButton.Name = "shootButton";
+            this.shootButton.Size = new System.Drawing.Size(75, 23);
+            this.shootButton.TabIndex = 4;
+            this.shootButton.Text = "Shoot";
+            this.shootButton.UseVisualStyleBackColor = true;
+            // 
+            // rematchButton
+            // 
+            this.rematchButton.Location = new System.Drawing.Point(469, 172);
+            this.rematchButton.Name = "rematchButton";
+            this.rematchButton.Size = new System.Drawing.Size(75, 23);
+            this.rematchButton.TabIndex = 5;
+            this.rematchButton.Text = "Rematch";
+            this.rematchButton.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(137, 37);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(36, 38);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(6, 39);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(49, 33);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(71, 37);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(45, 35);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(82, 37);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(45, 35);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 5;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(17, 39);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(49, 33);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox6.Image")));
+            this.pictureBox6.Location = new System.Drawing.Point(148, 37);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(36, 38);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox6.TabIndex = 3;
+            this.pictureBox6.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1004, 478);
+            this.Controls.Add(this.rematchButton);
+            this.Controls.Add(this.shootButton);
+            this.Controls.Add(this.versusLabel);
+            this.Controls.Add(this.outcomegroupBox);
+            this.Controls.Add(this.aigroupBox);
+            this.Controls.Add(this.playergroupBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.playergroupBox.ResumeLayout(false);
+            this.aigroupBox.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private GroupBox playergroupBox;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox1;
+        private GroupBox aigroupBox;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox5;
+        private PictureBox pictureBox6;
+        private GroupBox outcomegroupBox;
+        private Label versusLabel;
+        private Button shootButton;
+        private Button rematchButton;
+    }
+}
---
+++ RockPaperScissors-TM/Form1.cs
@@ -0,0 +1,10 @@
+namespace RockPaperScissors_TM
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
